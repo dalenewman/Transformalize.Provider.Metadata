f499b1902d34fa44b10664a216886a2ed981d5fa
Dale Newman <dalenewman@gmail.com>
2019-10-07 16:54:10 -0400

    updated dependencies, no fix for metadata vulnerability yet

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,5 +1,5 @@
 nuget pack Transformalize.Transform.Metadata.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Transform.Metadata.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-REM nuget push "c:\temp\modules\Transformalize.Transform.Metadata.0.6.9-beta.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget push "c:\temp\modules\Transformalize.Transform.Metadata.Autofac.0.6.9-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Metadata.0.6.19-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.Metadata.Autofac.0.6.19-beta.nupkg" -source https://api.nuget.org/v3/index.json
